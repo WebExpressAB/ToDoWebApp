256968b5e08c66f2b737841b0d227ff0b2ef2966
matildaahlberg <matilda.ahlberg@webexpress.se>
2024-12-16 15:00:34 +0100

    lagt till så man ser i sidebaren vart man befinner sig

--- projectScript.js
+++ projectScript.js
@@ -12,6 +12,27 @@ function toggleMenu() {
     sidenav.classList.toggle('active');
 }
 
+// Hämta alla länkar med klassen "sideA"
+var sideLinks = document.getElementsByClassName("sideA");
+
+// Loop genom alla länkar
+for (var i = 0; i < sideLinks.length; i++) {
+    sideLinks[i].addEventListener("click", function(event) {
+        // Förhindra att länken faktiskt följer sin standardhandling (navigerar)
+        event.preventDefault();
+
+        // Ta bort 'active' från alla länkar
+        for (var j = 0; j < sideLinks.length; j++) {
+            sideLinks[j].classList.remove("active");
+        }
+        
+        // Lägg till 'active' på den länk som klickades på
+        this.classList.add("active");
+
+        // Navigera till den länkade sidan efter att ha lagt till 'active'
+        window.location.href = this.href;
+    });
+}
 var projects = [];
 
 //Läs in vid sidans start innehåll i local storage
@@ -25,7 +46,7 @@ function uppdateraOutput() {
         output += 
         "   <div class='card4'>" +
         "<div class='projectCol'>" +
-            "<h2>" + projects[i] + "<h2/>" + " <input type='text' placeholder='Task...'>" + " <button>Add</button>" + "</div>" +"</div>"
+            "<h2>" + projects[i] + "<h2/>" + " <input type='text' id='projectInput${i}' placeholder='Task...'>" + " <button onclick='laggaTillProjectTask(${i})'>Add</button>" + "<ul id='outputProject${i}'>" + "</ul>" +"</div>" + "</div>"
     }
     document.getElementById("output").innerHTML = output;
 }
@@ -48,6 +69,38 @@ function lasFranLocalStorage() {
         
         document.getElementById("myInput").value = "";
         uppdateraOutput();
+        sparaTillLocalStorage(); // Spara till localStorage
+    }
+}
+
+//Funktion för att skriva ut arrayen i början
+function uppdateraProjectOutput(projectIndex) {
+    var outputProject = "";
+    for (var j = 0; j < projects[projectIndex].tasks.length; j++) {
+        outputProject += `
+               <li>
+                <input type='checkbox'>
+                ${projects[projectIndex].tasks[j]}
+                <i class='fa-regular fa-pen-to-square' onclick='redigera("${projectIndex}", ${j})'></i>
+                <i class='fa-solid fa-trash' onclick='taBortTask("${projectIndex}", ${j})'></i>
+            </li>
+        `;
+
+    }
+    document.getElementById("outputProject").innerHTML = outputProject;
+}
+function sparaTillLocalStorage() {
+    localStorage.setItem("projects[i]", JSON.stringify(projects[i]));
+}
+
+  // Funktion för att lägga till 
+  function laggaTillProjectTask() {
+    let projectInput = document.getElementById("projectInput").value;
+    if (projectInput.length !== 0) {
+            projects[i].push(projectInput);
+        
+        document.getElementById("projectInput").value = "";
+        uppdateraprojectOutput();
         sparaTillLocalStorage(); // Spara till localStorage
     }
 }
